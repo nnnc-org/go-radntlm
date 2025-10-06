7b530ffb782f8d1426915fd6a7810adce6ec4f6e
Cody Ernesti <cernesti@nnnc.org>
2025-10-06 08:53:34 -0500

    remove ability to populate flatfile from gen command

--- cmd/gen.go
+++ cmd/gen.go
@@ -1,14 +1,13 @@
 package cmd
 
-
 import (
+	"encoding/hex"
 	"fmt"
 	"os"
 	"time"
-	"encoding/hex"
 
-	"github.com/spf13/cobra"
 	"github.com/pterm/pterm"
+	"github.com/spf13/cobra"
 
 	"github.com/nnnc-org/go-radntlm/internal/crypto"
 )
@@ -16,8 +15,8 @@ import (
 var genCmd = &cobra.Command{
 	Use:     "generate",
 	Aliases: []string{"g", "gen"},
-	Short:   "Generate an NT Hash or flatfile entry",
-	Long: ``,
+	Short:   "Generate an NT Hash",
+	Long:    ``,
 	Run: func(cmd *cobra.Command, args []string) {
 		usr, _ := cmd.Flags().GetString("username")
 		pwd, _ := cmd.Flags().GetString("password")
@@ -31,30 +30,9 @@ var genCmd = &cobra.Command{
 
 		if usr == "" {
 			fmt.Println(hash)
-			os.Exit(0)
-		}
-
-		// Generate flatfile entry
-		// Format: username:hash:expiration
-		file, _ := cmd.Flags().GetString("file")
-		if file != "" {
-		    f, err := os.OpenFile(file, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-			if err != nil {
-				pterm.Error.Println(err)
-				os.Exit(1)
-			}
-			defer f.Close()
-
-			// append to file
-			_, err = f.Write([]byte(fmt.Sprintf("%s:%s:%d\n", usr, hash, time.Now().AddDate(1, 0, 0).Unix())))
-			if err != nil {
-				pterm.Error.Println(err)
-				os.Exit(1)
-			}
 		} else {
 			fmt.Printf("%s:%s:%d\n", usr, hash, time.Now().AddDate(1, 0, 0).Unix())
 		}
-
 		os.Exit(0)
 	},
 }
@@ -62,6 +40,6 @@ var genCmd = &cobra.Command{
 func init() {
 	rootCmd.AddCommand(genCmd)
 
-	genCmd.Flags().StringP("username", "u", "", "Username")
+	genCmd.Flags().StringP("username", "u", "", "Username (optional)")
 	genCmd.Flags().StringP("password", "p", "", "Plaintext password")
 }
